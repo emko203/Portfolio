38b0cf41ab0f99be5e2f8a49359008fce2ec3262
Emanuil Karapachov <406743@student.fontys.nl>
2021-07-02 14:27:58 +0300

    Smooth Scrolling, ShowCV and proper links

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const nameInput = $('#name');
+const emailInput = $('#email');
+const subjectInput = $('#subject');
+const messageInput = $('#message');
+
+let name = sessionStorage.getItem('name');
+let email = sessionStorage.getItem('email');
+let phone = sessionStorage.getItem('subject');
+let message = sessionStorage.getItem('message');
+
+nameInput.on('change', () => {
+	sessionStorage.setItem('name', nameInput.val());
+});
+
+emailInput.on('change', () => {
+	sessionStorage.setItem('email', emailInput.val());
+});
+
+subjectInput.on('change', () => {
+	sessionStorage.setItem('subject', subjectInput.val());
+});
+
+messageInput.on('change', () => {
+	sessionStorage.setItem('message', messageInput.val());
+});
+
+//Modal
+const modal = $('.modal');
+
+window.onclick = (event) => {
+	if ($(event.target).is(modal)) {
+		modal.css('display', 'none');
+	}
+};
+
+const cvModal = $('div#cvDIV');
+
+function showCV() {
+	cvModal.css('display', 'flex');
+}
+
 // Flowing navbar
 $(document).ready(function() {
 	$(window).scroll(function() {
